787c0cddf05068ca016ba7cb56f74ed6a3f304f7
Kevin Keller <flygeneticist@gmail.com>
2015-02-21 20:50:07 -0500

    added basic node sever.

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res){
+    fs.readFile('index.html', function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+});
+
+var port = 8080;
+server.listen(port, function (){
+    console.log('Server running at: http://localhost:'+port);
+});
